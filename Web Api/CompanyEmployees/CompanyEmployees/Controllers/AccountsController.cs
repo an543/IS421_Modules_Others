94961ff8ab7c6cb998bb868f854e4f939d89f20b
an543 <46980112+an543@users.noreply.github.com>
2021-05-04 02:16:42 -0400

    Add files via upload

---
+++ Web Api/CompanyEmployees/CompanyEmployees/Controllers/AccountsController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CompanyEmployees.JwtFeatures;
+using Entities.DTO;
+using Entities.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/accounts")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager; 
+        private readonly IMapper _mapper;
+        private readonly JwtHandler _jwtHandler;
+
+        public AccountsController(UserManager<User> userManager, IMapper mapper, JwtHandler jwtHandler) 
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+            _jwtHandler = jwtHandler;
+        }
+
+        [HttpPost("Registration")] 
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration) 
+        {
+            if (userForRegistration == null || !ModelState.IsValid) 
+                return BadRequest(); 
+            
+            var user = _mapper.Map<User>(userForRegistration);
+
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password); 
+            if (!result.Succeeded) 
+            { 
+                var errors = result.Errors.Select(e => e.Description); 
+                
+                return BadRequest(new RegistrationResponseDto { Errors = errors }); 
+            }
+
+            await _userManager.AddToRoleAsync(user, "Viewer");
+
+            return StatusCode(201); 
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            var user = await _userManager.FindByNameAsync(userForAuthentication.Email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, userForAuthentication.Password))
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Invalid Authentication" });
+
+            var signingCredentials = _jwtHandler.GetSigningCredentials();
+            var claims = await _jwtHandler.GetClaims(user);
+            var tokenOptions = _jwtHandler.GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+    }
+}
